6cfb7b9d3afad5b2f04b11ece147f125ec29d029
Daniel Langio <langio.tehiniavo@gmail.com>
2025-05-30 21:20:50 +0300

    spec(core.entity): specify movement application method for Body

--- src/main/java/agfj/core/entity/Body.java
+++ src/main/java/agfj/core/entity/Body.java
@@ -4,4 +4,11 @@ package agfj.core.entity;
  * Represent a character's body
  * */
 public class Body extends Entity {
+
+    /**
+     * Applies a movement to the body
+     * */
+    public void applyMovement(Movement movement) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
