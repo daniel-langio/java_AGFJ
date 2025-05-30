00c38a55d3f523e3561488bb9674f3349d96118f
Daniel Langio <langio.tehiniavo@gmail.com>
2025-05-30 21:24:29 +0300

    chore(core.entity): add more context to Speech
    Added properties content and mood to core.entity.Speech

--- src/main/java/agfj/core/entity/Speech.java
+++ src/main/java/agfj/core/entity/Speech.java
@@ -1,7 +1,14 @@
 package agfj.core.entity;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 /**
  * Represent a Personality's expression
  * */
+@EqualsAndHashCode(callSuper = true)
+@Data
 public final class Speech extends Action {
+    private final String content;
+    private final CharacterMood mood;
 }
