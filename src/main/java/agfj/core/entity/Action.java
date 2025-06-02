db583090e2a4a3a2af4d74297a48b48cb8b5e01a
Daniel Langio <langio.tehiniavo@gmail.com>
2025-06-02 16:09:34 +0300

    chore(core.entity): add context to the action classes
    Moved the attribute speed to the Mother class core.entity.Action
    Created constructors for the daughter class of core.entity.Action

--- src/main/java/agfj/core/entity/Action.java
+++ src/main/java/agfj/core/entity/Action.java
@@ -1,7 +1,14 @@
 package agfj.core.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Represent an Entity's action
  * */
+@Getter
+@Setter
 public abstract sealed class Action permits Movement, Speech {
+    /** The speed which the action will be executed */
+    protected int speed;
 }
--- src/main/java/agfj/core/entity/Movement.java
+++ src/main/java/agfj/core/entity/Movement.java
@@ -1,16 +1,19 @@
 package agfj.core.entity;
 
 import agfj.core.physics.Vector2D;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
  * Represents a body's movement
  * */
-@AllArgsConstructor
 @Getter
 public final class Movement extends Action {
-    private final int speed;
     private final int rotation;
     private final Vector2D velocity;
+
+    public Movement(int speed, int rotation,Vector2D velocity) {
+        this.speed = speed;
+        this.rotation = rotation;
+        this.velocity = velocity;
+    }
 }
