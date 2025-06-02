8ef990b05ce21e92f184c6c87f0ba75d597062cc
Daniel Langio <langio.tehiniavo@gmail.com>
2025-06-02 15:54:35 +0300

    feat(core.physics): create coordinate and position

---
+++ src/main/java/agfj/core/physics/Coordinate2d.java
@@ -0,0 +1,14 @@
+package agfj.core.physics;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Represent a coordinate in a 2D space
+ * */
+@AllArgsConstructor
+@Data
+public class Coordinate2d {
+    protected double x;
+    protected double y;
+}
---
+++ src/main/java/agfj/core/physics/Position.java
@@ -0,0 +1,14 @@
+package agfj.core.physics;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Represent the position of a body in a 2d space
+ * */
+@AllArgsConstructor
+@Data
+public class Position {
+    private int x;
+    private int y;
+}
