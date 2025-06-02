73c1580a575aafaa66ebb543d27cfe77e997f6e4
Daniel Langio <langio.tehiniavo@gmail.com>
2025-06-02 15:55:46 +0300

    chore(core.entity): add state and position properties to Body

--- src/main/java/agfj/core/entity/Body.java
+++ src/main/java/agfj/core/entity/Body.java
@@ -1,9 +1,13 @@
 package agfj.core.entity;
 
+import agfj.core.physics.Position;
+
 /**
  * Represent a character's body
  * */
 public class Body extends Entity {
+    private BodyState state;
+    private Position position;
 
     /**
      * Applies a movement to the body
