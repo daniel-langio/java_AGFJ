52007b85cdd865607796ba4b95b263330d1e2cdb
Daniel Langio <langio.tehiniavo@gmail.com>
2025-06-05 00:32:36 +0300

    spec(core.entity): add comment to the Movement class

--- src/main/java/agfj/core/entity/Movement.java
+++ src/main/java/agfj/core/entity/Movement.java
@@ -8,9 +8,17 @@ import lombok.Getter;
  * */
 @Getter
 public final class Movement extends Action {
+
+    /** The rotation value to apply */
     private final int rotation;
+
+    /** The vector to add to the position */
     private final Vector2D velocity;
 
+    /**
+     * @param speed         the speed to execute the movement
+     * @param rotation      the rotation to apply
+     * @param velocity      the vector to add to the position */
     public Movement(int speed, int rotation,Vector2D velocity) {
         this.speed = speed;
         this.rotation = rotation;
