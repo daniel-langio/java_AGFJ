3fa842ba678451e1398f9ab4a0df19a5e976120f
Daniel Langio <langio.tehiniavo@gmail.com>
2025-06-05 00:23:25 +0300

    spec(core.entity): add comment and contexts to the CharacterState class

--- src/main/java/agfj/core/entity/CharacterState.java
+++ src/main/java/agfj/core/entity/CharacterState.java
@@ -1,9 +1,18 @@
 package agfj.core.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * Represent the state if a character
  * */
+@AllArgsConstructor
+@Data
 public class CharacterState {
+
+    /** The body state */
     private BodyState bodyState;
+
+    /** The executed action. Null if no action is executed */
     private Action action;
 }
