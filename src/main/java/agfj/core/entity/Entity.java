46883168653271861fceb9137837865dc93cfbbc
Daniel Langio <langio.tehiniavo@gmail.com>
2025-06-11 07:39:49 +0300

    spec(core.entity): add comment to the entity class

--- src/main/java/agfj/core/entity/Entity.java
+++ src/main/java/agfj/core/entity/Entity.java
@@ -4,6 +4,10 @@ package agfj.core.entity;
  * Base class for a game object
  * */
 public abstract class Entity {
+
+    /** The name of the entity */
     protected String name;
+
+    /** A description of the entity */
     protected String description;
 }
