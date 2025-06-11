bf94ccb3c74a9508b3d00d16c6b7299b579577ec
Phylosius <hei.phylosius@gmail.com>
2025-06-11 07:21:47 +0300

    chore(core.entity): add context to the body state class

--- src/main/java/agfj/core/entity/BodyState.java
+++ src/main/java/agfj/core/entity/BodyState.java
@@ -4,4 +4,16 @@ package agfj.core.entity;
  * Represent the state of a body
  * */
 public class BodyState {
+
+    /** The maximum health point */
+    private int maxHealthPoint;
+
+    /** The current health point */
+    private int actualHealthPoint;
+
+    /** The current cleanness point */
+    private int maxCleannessPoint
+
+    /** The current cleanness point */
+    private int actualCleannessPoint
 }
