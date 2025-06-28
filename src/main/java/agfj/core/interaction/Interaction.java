a8b9ee3625a3682d7b51c30387c15f66bd7c671f
Daniel Langio <langio.tehiniavo@gmail.com>
2025-06-28 10:51:25 +0300

    chore(core.interaction): use character state range in interaction targeting

--- src/main/java/agfj/core/interaction/Interaction.java
+++ src/main/java/agfj/core/interaction/Interaction.java
@@ -1,15 +1,15 @@
 package agfj.core.interaction;
 
 import agfj.core.entity.Character;
-import agfj.core.entity.CharacterState;
+import agfj.core.entity.CharacterStateRange;
 
 /**
  * Represent an action to do for a specific character state
  * */
-public record Interaction (CharacterState targetState, Action action) {
+public record Interaction (CharacterStateRange targetState, Action action) {
     
     /** Tell if a character matches the target's state*/
     public Boolean matches(Character target) {
-        return this.targetState.equals(target.getState());
+        return this.targetState.isInclude(target.getState());
     }
 }
