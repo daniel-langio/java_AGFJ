ec0e8331dc65d74910d0df35247ff052c3f19458
Daniel Langio <langio.tehiniavo@gmail.com>
2025-09-16 20:34:02 +0300

    chore: update format.sh

--- format.sh
+++ format.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-find src -name "*.java" -print0 | xargs -0 -n 500 java -jar google-java-format-1.23.0-all-deps.jar --replace
+find core -name "*.java" -print0 | xargs -0 -n 500 java -jar google-java-format-1.23.0-all-deps.jar --replace
+find html -name "*.java" -print0 | xargs -0 -n 500 java -jar google-java-format-1.23.0-all-deps.jar --replace
+find lwjgl3 -name "*.java" -print0 | xargs -0 -n 500 java -jar google-java-format-1.23.0-all-deps.jar --replace
