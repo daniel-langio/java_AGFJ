53ab27ea8fdd7b5db948d3e0fda10ca069c965f0
Daniel Langio <langio.tehiniavo@gmail.com>
2025-10-17 20:14:00 +0300

    chore(MyGame): testing code

--- core/src/main/java/vendredi/soir/agfj/game/MyGame.java
+++ core/src/main/java/vendredi/soir/agfj/game/MyGame.java
@@ -2,9 +2,28 @@ package vendredi.soir.agfj.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import java.util.List;
+import vendredi.soir.agfj.entity.AnimatedEntity;
 import vendredi.soir.agfj.entity.TexturedEntity;
+import vendredi.soir.agfj.graphics.sprites.AnimationCategory;
+import vendredi.soir.agfj.graphics.sprites.SpriteAnimation;
+import vendredi.soir.agfj.graphics.sprites.SpriteLoader;
 
-public class MyGame extends Game{
+public class MyGame extends Game {
+  private static final TexturedEntity player =
+      new TexturedEntity(
+          "32x32",
+          new Texture(Gdx.files.internal("sprites/prototypes/platforms/Tiles/Tile_10.png")));
+  private static final AnimatedEntity animatedPlayer =
+      new AnimatedEntity(
+          "Animated",
+          List.of(
+              new SpriteAnimation(
+                  0,
+                  30,
+                  AnimationCategory.IDLE,
+                  SpriteLoader.loadSpriteSet(
+                      "sprites/prototypes/character/Animations/Walking.png", 128, 128, 0, 0, 12))));
 
   public MyGame() {
     super();
@@ -12,6 +31,13 @@ public class MyGame extends Game{
 
   @Override
   public void init() {
-    world.addEntity(new TexturedEntity("32x32", new Texture( Gdx.files.internal("sprites/prototypes/platforms/Tiles/Tile_10.png"))));
+    world.addEntity(player);
+    world.addEntity(animatedPlayer);
+  }
+
+  @Override
+  public void logic() {
+    super.logic();
+    player.setX(player.getX() + 0.5f);
   }
 }
